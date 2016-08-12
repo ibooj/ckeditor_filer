40c14dd0875833760b1a75e9282a4750b036b3f4
Артем Большов <ibooj@ya.ru>
2016-08-12 13:54:43 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ckeditor_filer',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ibooj',
+    author_email='ibooj@ya.ru',
+    description='Django widget ckeditor whit support django-filer'
+)
